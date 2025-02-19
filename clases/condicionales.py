ee19ea74555513ad39db02c6d826316d9056cf6d
Sergio <you@example.com>
2025-02-19 20:37:21 +0100

    mas ejercicios

--- clases/condicionales.py
+++ clases/condicionales.py
@@ -1,7 +1,9 @@
-numero = 3
+numero = int(input("Introduce un número: "))
 
 if numero <2:
-    print("El número es menor que 2")   
+    print("El número es menor que 2")
+elif numero > 4:
+        print("Numero superior a 4")   
 else:
     print("El número es mayor o igual que 2")
 
